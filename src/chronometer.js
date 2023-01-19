9df3c9ae14ca0dfafc3dc54cc74c0812dfe9f5ac
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-01-19 11:11:50 -0500

    bonus iteration done

--- src/chronometer.js
+++ src/chronometer.js
@@ -6,7 +6,8 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-  setInterval(()=> {
+
+    this.intervalId = setInterval(()=> {
     if (typeof printTimeCallback === "function") {
       printTimeCallback();
     }
@@ -17,26 +18,41 @@ class Chronometer {
   }
 
   getMinutes() {
+    return Math.floor(this.currentTime/60)
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime % 60
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+
+    if (String(value).length === 1) {
+      return `0${value}`
+    } else {
+      return String(value)
+    }
     // ... your code goes here
   }
 
   stop() {
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0
     // ... your code goes here
   }
 
   split() {
+
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+
+    return `${minutes}:${seconds}`
     // ... your code goes here
   }
 }
